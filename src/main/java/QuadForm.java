ad24a0c446714c69b745ae90c3a9442efd96f432
Jack Chen <23hchen@athenian.org>
2022-02-17 12:34:47 -0800

    :)

--- src/main/java/QuadForm.java
+++ src/main/java/QuadForm.java
@@ -13,8 +13,20 @@ public class QuadForm {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //3. Create components and put them in the frame.
+        JLabel label = new JLabel ("ax^2 + bx + c = 0");
+        frame.add(label);
+        LTPanel ltenetwork = new LTPanel ("a =", 10);
+        frame.add(ltenetwork);
+        LTPanel ltenetwork1 = new LTPanel ("b =", 10);
+        frame.add(ltenetwork1);
+        LTPanel ltenetwork2 = new LTPanel ("c =", 10);
+        frame.add(ltenetwork2);
 
-        // YOUR CODE HERE
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
+        frame.add(panel1);
+
+        JButton button1 = new JButton()
 
         //4. Size the frame.
         // frame.setSize(400, 400);
